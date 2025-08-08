60040c019bba8870228e1a02716902f92b92b08a
Артём Анисимов <106230904+Artem-Anisimoff@users.noreply.github.com>
2025-08-08 16:23:50 +0400

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -53,3 +53,79 @@ group by
 	EXTRACT(ISODOW FROM s.sale_date)
 order by EXTRACT(ISODOW FROM s.sale_date), --соритровка по порядку дней недели
 income desc
+
+
+/*----------------------6шаг---------------------------*/
+
+with young as (
+	select age
+	from customers
+	where age between 16 and 25
+),                              --разделение на группы
+average as (
+	select age
+	from customers
+	where age between 26 and 40
+),
+old as (
+	select age
+	from customers
+	where age between 41 and 100
+)
+select                          --вывод групп
+	'16-25' as age_category,
+	count(age) as age_count
+from young 						
+
+union all
+
+select 
+	'26-40' as age_category,
+	count(age) as age_count
+from average 
+
+union all
+
+select 
+	'41+' as age_category,
+	count(age) as age_count
+from old
+
+
+
+/*---------------------------------------------------------------*/
+
+select 
+	TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month, --выбор даты в формате год-месяц
+	count(s.customer_id ) as total_customers, --кол-во покупателей
+	round(sum(s.quantity * p.price ), 0) as income 
+from sales as s
+join products as p 
+	on s.product_id = p.product_id 
+group by TO_CHAR(sale_date, 'YYYY-MM') --группировка по дате
+order by TO_CHAR(sale_date, 'YYYY-MM')
+
+/*----------------------------------------------------------------*/
+
+
+with sale_number as (
+select 
+	s.customer_id,
+	s.sale_date,
+	s.sales_person_id,
+	ROW_NUMBER() OVER (partition by s.customer_id order by sale_date) AS sale_number
+from sales as s
+join products as p 
+	on s.product_id = p.product_id 
+where p.price = 0
+) --нумерация всех 0 покупок
+
+select 
+	CONCAT(c.first_name,' ',c.last_name) as customer,
+	sn.sale_date,
+	CONCAT(e.first_name,' ',e.last_name) as seller
+from sale_number sn
+join customers c on sn.customer_id = c.customer_id
+join employees e on sn.sales_person_id = e.employee_id
+where sn.sale_number = 1 --выбор 1 нулевой покупки
+order by sn.customer_id
