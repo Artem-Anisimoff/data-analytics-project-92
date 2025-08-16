18c28c18ee8dc18c4512ba52443826f2709125bc
Артём Анисимов <106230904+Artem-Anisimoff@users.noreply.github.com>
2025-08-16 14:42:54 +0400

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,139 +1,178 @@
-SELECT 
-	COUNT(customer_id) as customers_count
-FROM customers;
+SELECT
+    COUNT(customer_id) AS customers_count
+FROM
+    customers;
 
-/*запрос считает количество покупателей*/
+/* запрос считает количество покупателей */
 
-/*---------5шаг-------------*/
+/* ---------5шаг------------- */
 
-/*top_10_total_income.csv*/
-select 
-	CONCAT(emp.first_name,' ',emp.last_name) as seller, --выбор имени фамилии
-	count(s.sales_person_id) as operations, --кол-во операций
-	trunc(sum(s.quantity * p.price ), 0) as income --выручка
-from sales as s
-join employees as emp 
-	on s.sales_person_id=emp.employee_id  
-join products as p 
-	on s.product_id=p.product_id  
-group by emp.first_name , emp.last_name 
-order by income desc
-limit 10
-/***/
+/* top_10_total_income.csv */
+SELECT
+    CONCAT(emp.first_name, ' ', emp.last_name) AS seller,
+    COUNT(s.sales_person_id) AS operations,
+    TRUNC(SUM(s.quantity * p.price), 0) AS income
+FROM
+    sales AS s
+INNER JOIN
+    employees AS emp
+    ON s.sales_person_id = emp.employee_id
+INNER JOIN
+    products AS p
+    ON s.product_id = p.product_id
+GROUP BY
+    emp.first_name,
+    emp.last_name
+ORDER BY
+    income DESC
+LIMIT 10;
 
-/* lowest_average_income.csv*/
-with seller_tab  as (
-	select 
-		CONCAT(e.first_name,' ',e.last_name) as seller, --выбор имени фамилии
-		trunc(AVG(s.quantity * p.price), 0) as average_income --средняя выручка за продажу
-	from sales as s
-	join employees as e
-		on s.sales_person_id=e.employee_id  
-	join products as p 
-		on s.product_id=p.product_id  
-	group by e.first_name , e.last_name
+/* lowest_average_income.csv */
+WITH seller_tab AS (
+    SELECT
+        CONCAT(e.first_name, ' ', e.last_name) AS seller,
+        TRUNC(AVG(s.quantity * p.price), 0) AS average_income
+    FROM
+        sales AS s
+    INNER JOIN
+        employees AS e
+        ON s.sales_person_id = e.employee_id
+    INNER JOIN
+        products AS p
+        ON s.product_id = p.product_id
+    GROUP BY
+        e.first_name,
+        e.last_name
 ),
-seller_avg as (
-	select 
-		trunc(AVG(s.quantity * p.price), 0) as all_average
-	from sales as s
-	join products as p 
-		on s.product_id=p.product_id
+seller_avg AS (
+    SELECT
+        TRUNC(AVG(s.quantity * p.price), 0) AS all_average
+    FROM
+        sales AS s
+    INNER JOIN
+        products AS p
+        ON s.product_id = p.product_id
 )
-select 
-st.seller, 
-st.average_income
-from seller_tab st
-where st.average_income < (select all_average from seller_avg)
-order by st.average_income 
+SELECT
+    st.seller,
+    st.average_income
+FROM
+    seller_tab st
+WHERE
+    st.average_income < (SELECT all_average FROM seller_avg)
+ORDER BY
+    st.average_income;
 
-/*day_of_the_week_income.csv*/
-select 
-	CONCAT(emp.first_name,' ',emp.last_name) as seller, --выбор имени фамилии
-	TO_CHAR(s.sale_date, 'day') AS day_of_week, --выбор по дням недели
-	trunc(SUM(s.quantity * p.price), 0) as income --выбор суммы продаж в день недели
-from sales as s
-join employees as emp 
-	on s.sales_person_id=emp.employee_id  
-join products as p 
-	on s.product_id=p.product_id  
-group by 
-	emp.first_name ,
-	emp.last_name,
-	to_char(s.sale_date, 'day'),
-	EXTRACT(ISODOW FROM s.sale_date)
-order by EXTRACT(ISODOW FROM s.sale_date), --соритровка по порядку дней недели
-seller
-/***/
+/* day_of_the_week_income.csv */
+SELECT
+    CONCAT(emp.first_name, ' ', emp.last_name) AS seller,
+    TO_CHAR(s.sale_date, 'day') AS day_of_week,
+    TRUNC(SUM(s.quantity * p.price), 0) AS income
+FROM
+    sales AS s
+INNER JOIN
+    employees AS emp
+    ON s.sales_person_id = emp.employee_id
+INNER JOIN
+    products AS p
+    ON s.product_id = p.product_id
+GROUP BY
+    emp.first_name,
+    emp.last_name,
+    TO_CHAR(s.sale_date, 'day'),
+    EXTRACT(ISODOW FROM s.sale_date)
+ORDER BY
+    EXTRACT(ISODOW FROM s.sale_date),
+    seller;
 
-/*----------------------6шаг---------------------------*/
+/* ----------------------6шаг--------------------------- */
 
-/*age_groups.csv*/
-with young as (
-	select age
-	from customers
-	where age between 16 and 25
-),                              --разделение на группы
-average as (
-	select age
-	from customers
-	where age between 26 and 40
+/* age_groups.csv */
+WITH young AS (
+    SELECT
+        age
+    FROM
+        customers
+    WHERE
+        age BETWEEN 16 AND 25
 ),
-old as (
-	select age
-	from customers
-	where age between 41 and 100
+average AS (
+    SELECT
+        age
+    FROM
+        customers
+    WHERE
+        age BETWEEN 26 AND 40
+),
+old AS (
+    SELECT
+        age
+    FROM
+        customers
+    WHERE
+        age BETWEEN 41 AND 100
 )
-select                          --вывод групп
-	'16-25' as age_category,
-	count(age) as age_count
-from young 						
+SELECT
+    '16-25' AS age_category,
+    COUNT(age) AS age_count
+FROM
+    young
+UNION ALL
+SELECT
+    '26-40' AS age_category,
+    COUNT(age) AS age_count
+FROM
+    average
+UNION ALL
+SELECT
+    '40+' AS age_category,
+    COUNT(age) AS age_count
+FROM
+    old;
 
-union all
+/* customers_by_month.csv */
+SELECT
+    TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
+    COUNT(DISTINCT s.customer_id) AS total_customers,
+    TRUNC(SUM(s.quantity * p.price), 0) AS income
+FROM
+    sales AS s
+INNER JOIN
+    products AS p
+    ON s.product_id = p.product_id
+GROUP BY
+    TO_CHAR(sale_date, 'YYYY-MM')
+ORDER BY
+    TO_CHAR(sale_date, 'YYYY-MM');
 
-select 
-	'26-40' as age_category,
-	count(age) as age_count
-from average 
-
-union all
-
-select 
-	'40+' as age_category,
-	count(age) as age_count
-from OLD
-/***/
-
-/*customers_by_month.csv */
-select 
-	TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month, --выбор даты в формате год-месяц
-	count(DISTINCT s.customer_id ) as total_customers, --кол-во покупателей
-	trunc(sum(s.quantity * p.price ), 0) as income 
-from sales as s
-join products as p 
-	on s.product_id = p.product_id 
-group by TO_CHAR(sale_date, 'YYYY-MM') --группировка по дате
-order by TO_CHAR(sale_date, 'YYYY-MM')
-
-
-/*special_offer.csv */
-with sale_number as (
-select 
-	s.customer_id,
-	s.sale_date,
-	s.sales_person_id,
-	ROW_NUMBER() OVER (partition by s.customer_id order by sale_date) AS sale_number
-from sales as s
-join products as p 
-	on s.product_id = p.product_id 
-where p.price = 0
-) --нумерация всех 0 покупок
-select 
-	CONCAT(c.first_name,' ',c.last_name) as customer,
-	sn.sale_date,
-	CONCAT(e.first_name,' ',e.last_name) as seller
-from sale_number sn
-join customers c on sn.customer_id = c.customer_id
-join employees e on sn.sales_person_id = e.employee_id
-where sn.sale_number = 1 --выбор 1 нулевой покупки
-order by sn.customer_id
+/* special_offer.csv */
+WITH sale_number AS (
+    SELECT
+        s.customer_id,
+        s.sale_date,
+        s.sales_person_id,
+        ROW_NUMBER() OVER (PARTITION BY s.customer_id ORDER BY sale_date) AS sale_number
+    FROM
+        sales AS s
+    INNER JOIN
+        products AS p
+        ON s.product_id = p.product_id
+    WHERE
+        p.price = 0
+)
+SELECT
+    CONCAT(c.first_name, ' ', c.last_name) AS customer,
+    sn.sale_date,
+    CONCAT(e.first_name, ' ', e.last_name) AS seller
+FROM
+    sale_number sn
+INNER JOIN
+    customers c
+    ON sn.customer_id = c.customer_id
+INNER JOIN
+    employees e
+    ON sn.sales_person_id = e.employee_id
+WHERE
+    sn.sale_number = 1
+ORDER BY
+    sn.customer_id;
